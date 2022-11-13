2c7b733436383a8a9242a767e147048071f330f7
Arun Singh <aruns3435@gmail.com>
2022-11-13 10:53:46 +0530

    Login Register

--- beauty_bebo_clone/src/App.js
+++ beauty_bebo_clone/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
+// import Login from './Components/LoginRegister/Login';
+import Register from './Components/LoginRegister/Register';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <h3>Login and Register</h3>
+      <h3>Login</h3> */}
+      {/* <Login /> */}
+      <Register />
     </div>
   );
 }
